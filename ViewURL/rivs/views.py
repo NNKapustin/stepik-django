8677de5c5f574d454f5867723f81de5969a4989b
nikita <nikita.n.kapustin@gmail.com>
2023-06-19 23:16:39 +0300

    3.8 finished

--- ViewURL/rivs/views.py
+++ ViewURL/rivs/views.py
@@ -9,6 +9,20 @@ class Info:
     film: str
 
 
+people = [
+    {'name': 'Жанна Ивановна Бобылева', 'age': 28, 'phone': '+72609577301'},
+    {'name': 'Спиридон Феликсович Алексеев', 'age': 48, 'phone': '8 445 133 42 50'},
+    {'name': 'Лыткина Зоя Рубеновна', 'age': 34, 'phone': '84061070300'},
+    {'name': 'Олимпиада Святославовна Петухова', 'age': 70, 'phone': '8 740 992 96 95'},
+    {'name': 'Лазарева Нина Кирилловна', 'age': 67, 'phone': '89040731989'},
+    {'name': 'Каллистрат Ильич Ширяев', 'age': 63, 'phone': '+7 418 298 8976'},
+    {'name': 'Евсеев Любосмысл Чеславович', 'age': 47, 'phone': '83111461302'},
+    {'name': 'Прохор Харламович Артемьев', 'age': 47, 'phone': '+77827445919'},
+    {'name': 'Кондрат Игнатьевич Ершов', 'age': 35, 'phone': '+7 419 594 39 00'},
+    {'name': 'Ипат Власович Ильин', 'age': 47, 'phone': '88004779773'}
+]
+
+
 def index(request):
     info = Info(1964, 'Бейрут', 'На гребне волны')
     data = {
@@ -17,3 +31,10 @@ def index(request):
         'movie_name': info.film,
     }
     return render(request, 'rivs/index.html', context=data)
+
+
+def get_people_list(request):
+    data = {
+        'people': people
+    }
+    return render(request, 'rivs/people.html', context=data)

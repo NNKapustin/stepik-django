448a40ff2bba72c95750e30c7d913628490cef81
nikita <nikita.n.kapustin@gmail.com>
2023-06-24 21:29:45 +0300

    4.10 completed

---
+++ movie_proj/movie_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=40)
+    rating = models.IntegerField()
+    year = models.IntegerField(null=True)  # Может быть null
+    budget = models.IntegerField(default=1000000)
+
+    def __str__(self):
+        return f"{self.name} - {self.rating}%, {self.budget}, {self.year}"
---
+++ movie_proj/movie_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Movie
+
+
+def show_all_movies(request):
+    movies = Movie.objects.all()
+    return render(request, 'movie_app/all_movies.html', {'movies': movies})
